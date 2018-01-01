8b816996077e5c4ad7182abc9e2af02f41a8a956
Lucas Lopes Cunha <lucaslopes20@gmail.com>
2018-01-01 21:24:52 -0200

    Teste unitário com resultados do serviço

--- src/app/characters/characters.service.spec.ts
+++ src/app/characters/characters.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpTestingController,HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { CharactersService } from "./characters.service";
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
@@ -15,24 +15,75 @@ describe('CharactersService', () => {
         ReactiveFormsModule, FormsModule
       ],
       providers: [
-       CharactersService
+        CharactersService
       ]
     });
 
     charactersService = TestBed.get(CharactersService);
     httpMock = TestBed.get(HttpTestingController);
- 
+
   });
 
 
   it('should get the data successful', () => {
-    charactersService.getCharacter(10).subscribe((star :any) => {
+    charactersService.getCharacter(10).subscribe((star: any) => {
       expect(star['name']).toBe('Luke');
     });
-    const req = httpMock.expectOne(url+`people/10/`, 'call to api');
+    const req = httpMock.expectOne(url + `people/10/`, 'call to api');
     expect(req.request.method).toBe('GET');
     req.flush({
       name: 'Luke'
+    });
+
+    httpMock.verify();
+  });
+
+  it('should get the data successful', () => {
+    const species = 
+      {
+        count: '2',
+
+        results: [
+          {
+            name: "Luke Skywalker",
+            height: "172"
+          },
+          {
+            name: "C-3PO",
+            height: '167'
+          }]
+      };
+    charactersService.getCharacters().subscribe(char => {
+      expect(char['count']).toBe('2');
+      expect(char['results'][0]['name']).toBe(species['results'][0]['name']);
+    });
+    const req = httpMock.expectOne(url + `people/?page=1`, 'call to api');
+    expect(req.request.method).toBe('GET');
+    req.flush({
+      count: '2',
+
+      results: [
+        {
+          name: "Luke Skywalker",
+          height: "172"
+        },
+        {
+          name: "C-3PO",
+          height: 167
+        }]
+    });
+
+    httpMock.verify();
+  });
+  it('should get the data successful', () => {
+    charactersService.getSpecies(10).subscribe((star: any) => {
+      expect(star['name']).toBe('Sullustan');
+    });
+    const req = httpMock.expectOne(url + `species/10`, 'call to api');
+    expect(req.request.method).toBe('GET');
+    req.flush({
+      name: 'Sullustan'
+
     });
 
     httpMock.verify();
